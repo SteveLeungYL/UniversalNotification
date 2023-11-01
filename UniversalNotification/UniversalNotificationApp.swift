59757744ee59e1aa6442a8305166a1f4414b2ceb
Yu Liang <luy70@psu.edu>
2023-10-31 23:22:58 -0400

    Initial Commit

---
+++ UniversalNotification/UniversalNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversalNotificationApp.swift
+//  UniversalNotification
+//
+//  Created by Yu Liang on 10/31/23.
+//
+
+import SwiftUI
+
+@main
+struct UniversalNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
